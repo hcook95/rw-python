d0f7a84b9c5b5accbaac7cfd048aeb0492df6fa6
Hayden Cook <haydencook95@gmail.com>
2022-05-31 18:33:47 -0600

    added imports

--- rapidwright.py
+++ rapidwright.py
@@ -3,3 +3,7 @@ import jpype.imports
 from jpype.types import *
 
 jpype.startJVM(classpath=["rapidwright-2022.1.1-standalone-lin64.jar"])
+
+from com.xilinx.rapidwright.device import *
+from com.xilinx.rapidwright.design import *
+from com.xilinx.rapidwright.rwroute import *
